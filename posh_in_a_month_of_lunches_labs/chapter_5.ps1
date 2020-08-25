09f260b6e48c6f07a60499a976d0dada6a2efc7c
Sean Loughrey <s.a.loughrey@gmail.com>
2020-08-24 23:01:38 -0400

    Chapter 5 bonus section complete. Added a new Organizational Unit to the domain controller and created a new GPO to link to it.

--- posh_in_a_month_of_lunches_labs/chapter_5.ps1
+++ posh_in_a_month_of_lunches_labs/chapter_5.ps1
@@ -10,5 +10,26 @@ Get-Command -Module BestPractices
 Get-Help Get-BpaModel -full
 Get-Help Invoke-BpaModel -full
 
+# Figure out what model ids we need
+Get-BpaModel -ModelId * | ft
+
 # Run the analyzer for the Directory Services and DNS Server
-Get-BpaModel -ModelId Microsoft/Windows/DirectoryServices, Microsoft/Windows/DNSServer | Invoke-BpaModel
+Get-BpaModel -ModelId Microsoft/Windows/DirectoryServices, Microsoft/Windows/DNSServer | Invoke-BpaModel | ConvertTo-Html | Out-File -FilePath ".\lab_output\ds_dns_bpa_results.html"
+
+
+# Bonus work section
+
+# Figure out what commands are available relating to a GPO
+Get-Command -Module GroupPolicy
+
+# Make a new OU to play with
+$ouName = "Test OU 2"
+New-ADOrganizationalUnit -Name $ouName -Path "DC=sean,DC=local"
+
+# Make a new GPO
+$gpoName = "Test GPO 2"
+New-GPO -Name $gpoName
+
+# Link our GPO to an OU or update
+New-GPLink -Name $gpoName -Target "OU=$ouName,DC=sean,DC=local" -LinkEnabled Yes
+Set-GPLink -Name $gpoName -Target "OU=$ouName,DC=sean,DC=local" -LinkEnabled No
